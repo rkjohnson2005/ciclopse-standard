43371c978f7455f97938b648acf4428d16de55a9
Richard Johnson <development@rkjohn.com>
2018-10-15 10:49:21 -0400

    First Commit

---
+++ src/CICLOPSE/Standard.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Rick
+ * Date: 10/15/2018
+ * Time: 10:40 AM
+ */
+
+namespace CICLOPSE;
+
+
+class Standard implements \StandardInterface
+{
+    public function getLink($url, $display, $class = '')
+    {
+        // TODO: Implement getLink() method.
+    }
+}
---
+++ src/CICLOPSE/StandardInterface.php
@@ -0,0 +1,4 @@
+<?php
+interface StandardInterface {
+    public function getLink($url, $display, $class = '');
+}
