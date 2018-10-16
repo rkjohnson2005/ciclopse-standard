c1a30330d0f58196736c12b213c2bcfaf49b20d9
Richard Johnson <development@rkjohn.com>
2018-10-16 09:48:47 -0400

    Fixes

--- src/CICLOPSE/Database.php
+++ src/CICLOPSE/Database.php
@@ -7,14 +7,14 @@ class Database extends Standard
     public function select($database, $sql, $values = array())
     {
         try {
-            $db_handle = new \PDO("mysql:host=" . $database_credentials[$database]['server'] . ";dbname=" . $database . ";charset=utf8", $database_credentials[$database]['user'], $database_credentials[$database]['password']);
+            $db_handle = new \PDO("mysql:host=" . DATABASE_CREDENTIALS[$database]['server'] . ";dbname=" . $database . ";charset=utf8", DATABASE_CREDENTIALS[$database]['user'], DATABASE_CREDENTIALS[$database]['password']);
         } catch (\PDOException $exception) {
             trigger_error($exception->getMessage(), E_WARNING );
             exit();
         }
         $query = $db_handle->prepare($sql);
         if (!$query) {
-            trigger_error(print_r($db_handle->errorInfo(), true));
+            trigger_error(print_r($db_handle->errorInfo(), true), E_WARNING);
             exit();
         }
         $query->execute($values);
